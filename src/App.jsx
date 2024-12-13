33c565fc9bed99bbd86e64b4cce95a0f15cf1210
fatou fall <fatoufall95@gmail.com>
2024-12-12 19:22:48 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Homepage from "./components/Homepage";
+import EmployeePage from "./components/EmploeePage";
+
+function App() {
+  return (
+    <div className="app-container">
+      <Homepage />
+
+      <EmployeePage />
+    </div>
+  );
+}
+
+export default App;
